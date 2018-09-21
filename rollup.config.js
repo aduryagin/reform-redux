f7891b55ff001ffb99168cea6d7e8b5c1ff646e6
Alexey Duryagin <aiduryagih@gmail.com>
2018-09-21 12:26:39 +0300

    make commonJS bundle

--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
+import nodeResolve from 'rollup-plugin-node-resolve';
 
-const format = 'es';
 const common = {
   external: ['react', 'prop-types', 'immutable', 'redux'],
   plugins: [
@@ -26,22 +27,47 @@ const common = {
 };
 
 export default [
+  // ES modules
+
   {
+    ...common,
+    input: 'source/index.js',
+    output: {
+      name: 'reform-redux',
+      file: 'dist/reform-redux.es.js',
+      format: 'es',
+    },
+  },
+  {
+    ...common,
+    input: 'source/immutable.js',
+    output: {
+      name: 'reform-redux',
+      file: 'dist/immutable.es.js',
+      format: 'es',
+    },
+  },
+
+  // CommonJS
+
+  {
+    ...common,
     input: 'source/index.js',
     output: {
       name: 'reform-redux',
       file: 'dist/reform-redux.js',
-      format,
+      format: 'cjs',
     },
-    ...common,
+    plugins: [...common.plugins, nodeResolve(), commonjs()],
   },
   {
+    ...common,
     input: 'source/immutable.js',
     output: {
       name: 'reform-redux',
       file: 'dist/immutable.js',
-      format,
+      format: 'cjs',
     },
-    ...common,
+    plugins: [...common.plugins, nodeResolve(), commonjs()],
   },
 ];
